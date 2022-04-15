0d5b78fc7f77ca1903bd427b55a1921906106f84
ecriturebrailmaistupeuxrienvoir <90462132+ecriturebrailmaistupeuxrienvoir@users.noreply.github.com>
2022-04-15 15:59:54 +0200

    Create main_V1.py

---
+++ main_V1.py
@@ -0,0 +1,163 @@
+import pygame
+pygame.init()
+import sys
+import math
+import random
+
+display = pygame.display.set_mode((800, 600))
+clock = pygame.time.Clock()
+
+class Route :
+    def __init__ (self, x, y) :
+        self.x = x
+        self.y = y
+        self.width = 20
+        self.height = 20
+        self.color = (103, 113, 121)
+        self.infla = False
+
+    def main(self, display) :
+        pygame.draw.rect(display, self.color, (self.x, self.y, self.width, self.height))
+
+class Champs :
+    def __init__ (self, x, y) :
+        self.x = x
+        self.y = y
+        self.width = 20
+        self.height = 20
+        self.color = (0, 86, 27)
+        self.infla = True
+        self.etat = "eteint"
+        self.proba_combu = 500
+        self.temps_combu = random.randint(300, 600)
+        self.temps_brule = 0
+        self.adjacents = []
+    
+    def main(self, display) :
+        
+        self.adjacents = get_adj(self.x, self.y)
+            
+        if self.etat == "brule" :
+            action_feu(self, self.adjacents)
+        
+        if self.etat == "eteint" :
+            self.color = (0, 86, 27)
+        if self.etat == "brule" :
+            self.color = (187, 11, 11)
+        if self.etat == "consume" :
+            self.color = (70, 63, 50)
+        
+        pygame.draw.rect(display, self.color, (self.x, self.y, self.width, self.height))
+        
+class Bord :
+    def __init__ (self, x, y) :
+        self.x = x
+        self.y = y
+        self.width = 20
+        self.height = 20
+        self.color = (0, 0, 0)
+        self.infla = False
+
+    def main(self, display) :
+        pygame.draw.rect(display, self.color, (self.x, self.y, self.width, self.height))
+    
+
+def get_adj(x : int, y : int ) -> list :
+    """
+    Parameters
+    ----------
+    x : int
+        DESCRIPTION.
+    y : int
+        DESCRIPTION.
+
+    Returns
+    -------
+    list 
+        DESCRIPTION.
+
+    """
+    adj = []
+    x = int(x/20)
+    y = int(y/20)
+    #print(y, x, len(plateau), len(plateau[0]))
+    adj.append(plateau[y-1][x-1])
+    adj.append(plateau[y-1][x])
+    adj.append(plateau[y-1][x+1])
+    adj.append(plateau[y][x-1])
+    adj.append(plateau[y][x+1])
+    adj.append(plateau[y+1][x-1])
+    adj.append(plateau[y+1][x])
+    adj.append(plateau[y+1][x+1])
+    return adj
+
+
+def action_feu(self, adj : list) -> None :
+    self.temps_brule += 1
+    if self.temps_brule == self.temps_combu :
+        self.etat = "consume"
+        self.infla = False
+    for i in adj :
+        if i.infla :
+            if random.randint(0, i.proba_combu) == 42 :
+                i.etat = "brule"
+
+
+plateau = [[1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1], #initialisation de la matrice permettant la création du plateau
+           [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+           [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+           [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+           [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+           [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+           [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+           [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+           [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+           [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+           [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+           [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+           [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+           [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 2, 2, 1],
+           [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 2, 0, 0, 1],
+           [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 2, 0, 0, 0, 1],
+           [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 1],
+           [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 1],
+           [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 2, 2, 2, 0, 0, 0, 0, 1],
+           [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 2, 2, 2, 2, 2, 0, 0, 0, 0, 0, 0, 0, 1],
+           [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+           [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+           [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 2, 0, 2, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+           [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+           [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+           [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+           [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+           [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+           [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+           [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]]
+
+
+for i in range (len(plateau)) :
+    for j in range (len(plateau[1])) :
+        if plateau[i][j] == 0 :
+            plateau[i][j]=Champs(j*20, i*20)
+        if plateau[i][j] == 1 :
+            plateau[i][j]=Bord(j*20, i*20)
+        if plateau[i][j] == 2 :
+            plateau[i][j]=Route(j*20, i*20)
+
+plateau[18][3].etat = "brule"
+
+while True :    
+    mouse_x, mouse_y = pygame.mouse.get_pos() 
+
+    for event in pygame.event.get(): #Récupération des evenements
+        if event.type == pygame.QUIT : #Permet de quitter proprement pygame
+            sys.exit()
+    
+    for i in range (len(plateau)) :
+        for j in range (len(plateau[1])) :
+            plateau[i][j].main(display)
+    
+    test=plateau[12][13].adjacents
+    
+    clock.tick(60) #Permet de faire tourner le jeu à 60 fps
+    pygame.display.update() #Update la fenêtre
